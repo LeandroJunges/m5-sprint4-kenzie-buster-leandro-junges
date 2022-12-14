5b23056fde3d6b381d3e86964f808c6668cf0077
LeandroJunges <leandrojunges21@gmail.com>
2022-12-14 09:34:47 -0300

    refactor: Review code erasing untils files

--- users/permissions.py
+++ users/permissions.py
@@ -1,5 +1,6 @@
 from rest_framework import permissions
 from rest_framework.views import Request, View
+from users.models import User
 
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request: Request, view: View) -> bool:
@@ -15,5 +16,5 @@ class IsAdminOrReadOnly(permissions.BasePermission):
 
 
 class IsUserOrAdm(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
+    def has_object_permission(self, request:Request, view:View, obj:User):
         return request.user.is_authenticated and (request.user.is_employee or obj == request.user)
